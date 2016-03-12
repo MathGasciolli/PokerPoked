876204ed60167e3938feb3dd9efef92e8735dae9
Mathieu GASCIOLLI <gascio_m@epitech.eu>
2016-03-12 17:02:18 +0100

    work

--- jeu/export.c
+++ jeu/export.c
@@ -5,7 +5,7 @@
 ** Login   <gascio_m@epitech.net>
 **
 ** Started on  Sat Mar 12 01:34:44 2016 Mathieu GASCIOLLI
-** Last update Sat Mar 12 02:49:04 2016 Mathieu GASCIOLLI
+** Last update Sat Mar 12 17:00:12 2016 Mathieu GASCIOLLI
 */
 
 #include "poker.h"
@@ -27,7 +27,6 @@ void	export_config()
   curs_set(1);
   mvprintw(3, COLS/2 - 7, "EXPORT CONFIG");
   mvprintw(5, COLS/2 - 39, "Export your config to a file in order to load it easily for the next games !");
-  mvprintw(6, COLS/2 - 45, "If you want to load a config file, just use './poker [filename.poker]' when u start the game !");
   mvprintw(LINES/2 - 5, COLS/2 - 5, "File name:");
   mvscanw(LINES/2 - 4, COLS/2 - 5, "%[^\n]", title);
   curs_set(0);
